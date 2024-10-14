857992670da928315ce3a2d14c2c6dd8ac528dfa
zohrehchenari1995 <zohrehchenari2874@gmail.com>
2024-10-15 02:00:32 +0330

    added remove todo trash icon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add new todo
 
-const todos = [];
+let todos = [];
 const searchInput = document.querySelector(".search__input");
 const todoInput = document.querySelector(".todo-input");
 const todoList = document.querySelector(".todoList");
@@ -9,6 +9,7 @@ const selectFilter =document.querySelector(".todo__list");
 searchInput.addEventListener("submit",addNewTodos);
 selectFilter.addEventListener("change",filterTodos);
 
+// Add newtodos
 function addNewTodos(e){
     e.preventDefault();
     if(!todoInput.value) return null;
@@ -32,16 +33,22 @@ function createdTodos(todos){
       <li class="todo">
       <p class="todo__title">${todo.title}</p>
       <span class="todo__createAt">${new Date(todo.createdAt).toLocaleDateString("fa-IR")}</span>
-      <button data-todo-id =${todo.id}>
-          <i class="todo__checked  fa-solid fa-square-check"></i>
+      <button data-todo-id =${todo.id}  class="todo__checked">
+          <i class="fa-solid fa-square-check"></i>
       </button>
-      <button data-todo-id =${todo.id}>
-      <i class="todo__trash     fa-solid fa-trash-can"></i>
+      <button data-todo-id =${todo.id}  class="todo__trash">
+      <i class="fa-solid fa-trash-can"></i>
       </button>
     </li> `
   });
     todoList.innerHTML = result;
     todoInput.value = "";
+    // slelcted todo trash (beacuse no exist in dom)
+    const todoTrash = [...document.querySelectorAll(".todo__trash")];
+    todoTrash.forEach((btn)=>{
+      btn.addEventListener("click",removeTodos);
+
+    })
 };
 
 
@@ -67,3 +74,11 @@ function filterTodos(e){
     default: createdTodos(todos);
   }
 }
+
+// Remove todos(trash icon)
+function removeTodos(e){
+  const todoId = Number(e.target.dataset.todoId);
+  const filterTodo = todos.filter((t)=>{return t.id !== todoId});
+  todos =filterTodo;
+}
+
